39d86218d076929c3a92c9eabb90cf31aa3d59e0
Juuso Koivisto <juuso.oskari.koivisto@gmail.com>
2025-10-28 08:12:44 +0200

    parempi null checking

--- luku.php
+++ luku.php
@@ -7,15 +7,15 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()){
         echo "<tr>";
-        echo "<td class='sticky-element'>" . ($row["nimi"] == NULL ? "-" : $row["nimi"]) . "</td>";
-        echo "<td>" . ($row["paikka"] == NULL ? "-" : $row["paikka"]) . "</td>";
-        echo "<td>" . ($row["ohjaaja"] == NULL ? "-" : $row["ohjaaja"]) . "</td>";
-        echo "<td>" . ($row["yhteystiedot"] == NULL ? "-" : $row["yhteystiedot"]) . "</td>";
-        echo "<td>" . ($row["status"] == NULL ? "-" : $row["status"]) . "</td>";
-        echo "<td>" . ($row["aloitus"] == NULL ? "-" : $row["aloitus"]) . "</td>";
-        echo "<td>" . ($row["lopetus"] == NULL ? "-" : $row["lopetus"]) . "</td>";
-        echo "<td>" . ($row["ruokaraha"] == NULL ? "-" : $row["ruokaraha"]) . "</td>";
-        echo "<td>" . ($row["muuta"] == NULL ? "-" : $row["muuta"]) . "</td>";
+        echo "<td class='sticky-element'>" . ($row["nimi"] ?: "-") . "</td>";
+        echo "<td>" . ($row["paikka"] ?: "-") . "</td>";
+        echo "<td>" . ($row["ohjaaja"] ?: "-") . "</td>";
+        echo "<td>" . ($row["yhteystiedot"] ?: "-") . "</td>";
+        echo "<td>" . ($row["status"] ?: "-") . "</td>";
+        echo "<td>" . ($row["aloitus"] ?: "-") . "</td>";
+        echo "<td>" . ($row["lopetus"] ?: "-") . "</td>";
+        echo "<td>" . ($row["ruokaraha"] ?: "-") . "</td>";
+        echo "<td>" . ($row["muuta"] ?: "-") . "</td>";
         echo "</tr>";
     }
 } else {
