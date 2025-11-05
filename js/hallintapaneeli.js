335ea66dcfbea1d265565371a5a64289cc77e533
Juuso Koivisto <juuso.oskari.koivisto@gmail.com>
2025-11-05 14:00:23 +0200

    vaihda salasana

--- js/hallintapaneeli.js
+++ js/hallintapaneeli.js
@@ -39,18 +39,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const resets = document.querySelectorAll(".reset-btn");
     resets.forEach(deleteBtn => {
         resetBtn.addEventListener("click", function() {
-            deleteBtn.disabled = true;
-            const user = this.dataset.user;
-
-            fetch('actions/adminpanel/poista_kayttaja.php', {
-                method: 'POST',
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-                body: `user=${encodeURIComponent(user)}`
-            })
-            .then(res => res.text())
-            .then(response => console.log("Server:", response))
-            .catch(err => console.error("Fetch error:", err))
-            .finally(() => deleteBtn.disabled = false);
+            
         });
     });
 });
